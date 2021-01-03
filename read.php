44d4569293c6d6e286206e02946e53ba2a3f0628
braytonmaxwell <75170892+braytonmaxwell@users.noreply.github.com>
2021-01-03 18:41:20 +0800

    account.......

--- read.php
+++ read.php
@@ -1,3 +1,53 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM employees WHERE id = :id";
+    
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":id", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            if($stmt->rowCount() == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $salary = $row["salary"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    unset($stmt);
+    
+    // Close connection
+    unset($pdo);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -40,7 +90,6 @@
 
         h2 {
             color: gold;
-
         }
 
         label {
@@ -85,58 +134,26 @@
     </header>
     <!-- END | Header -->
 
-     <section class="item">
+    <section class="item">
         <div class="wrapper">
-        <h2>User Profile</h2>
-        <p>View or update profile</p>
+            <h2>User Profile</h2>
+            <p>View or update profile</p>
+            <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+            <div class="form-group">
+                        <label>Name</label>
+                        <p class="form-control-static"><?php echo $row["name"]; ?></p>
                     </div>
-                    <?php
-                    // Include config file
-                    require_once "config.php";
-                    
-                    // Attempt select query execution
-                    $sql = "SELECT * FROM userdetails";
-                    if($result = $pdo->query($sql)){
-                        if($result->rowCount() > 0){
-                            echo "<table class='table table-bordered font'>";
-                                echo "<thead>";
-                                    echo "<tr>";
-                                        echo "<th><font color=gold>Name</th>";
-                                        echo "<th><font color=gold>Email</th>";
-                                        echo "<th><font color=gold>Country</th>";
-                                        echo "<th><font color=gold>Phone</th>";
-                                        echo "<th><font color=gold>Password</th>";
-                                        echo "<th><font color=gold>Option</th>";
-                                    echo "</tr>";
-                                echo "</thead>";
-                                echo "<tbody>";
-                                while($row = $result->fetch()){
-                                    echo "<tr>";
-                                        echo "<td><font color=white>" . $row['username'] . "</td>";
-                                        echo "<td><font color=white>" . $row['email'] . "</td>";
-                                        echo "<td><font color=white>" . $row['country'] . "</td>";
-                                        echo "<td><font color=white>" . $row['phone'] . "</td>";
-                                        echo "<td><font color=white>" . $row['pasword'] . "</td>";
-                                        echo "<td>";
-                                            echo "<a href='read.php?id=". $row['id'] ."'class='btn btn-default''>View</a>";
-                                            echo "<a href='update.php?id=". $row['id'] ."'class='btn btn-primary''>Update</a>";
-                                        echo "</td>";
-                                    echo "</tr>";
-                                }
-                                echo "</tbody>";                            
-                            echo "</table>";
-                            // Free result set
-                            unset($result);
-                        } else{
-                            echo "<p class='lead'><em>No records were found.</em></p>";
-                        }
-                    } else{
-                        echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
-                    }
-                    
-                    // Close connection
-                    unset($pdo);
-                    ?>
+                    <div class="form-group">
+                        <label>Address</label>
+                        <p class="form-control-static"><?php echo $row["address"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Salary</label>
+                        <p class="form-control-static"><?php echo $row["salary"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Back</a>
+                    <a href="update.php" class="btn btn-primary">Update</a></p>
+            </form>
         </div>
     </section>
 
@@ -195,3 +212,5 @@
         </div>
     </footer>
 </body>
+
+</html>
